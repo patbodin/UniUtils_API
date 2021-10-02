50f25d7746af1cd7ee40b9130f2f04cb1b981932
Patbodin Intajak <patbodin30443@hotmail.com>
2021-10-02 14:11:51 +0700

    Try to test several types of accept header

--- routes/test.js
+++ routes/test.js
@@ -1,5 +1,6 @@
 const route = require("express").Router();
 
+//-- Test JSON
 route.get("/v1/testjson", (req, res) => {
     res.json({
         team: "eTopup"
@@ -11,6 +12,24 @@ route.get("/v2/testjson", (req, res) => {
         team: "ROM",
         dept: "FINE"
     });
+})
+
+//-- Test Format
+route.post("/v1/testformat", (req, res) => {
+    res.format({
+        'text/plain': () => {
+            res.status(200).send("Hello");
+        },
+        'text/html': () => {
+            res.status(200).send("<h2 style:color='red'>Hello</h2>");
+        },
+        'application/json': () => {
+            res.status(200).json({message: "Hello"});
+        },
+        default: function() {
+            res.status(406).send('Not Acceptable');
+        }
+    })
 })
 
 module.exports = route;
