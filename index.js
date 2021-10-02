31dcb755e2b14c92912bd2dabaf62e16a080ae01
Patbodin Intajak <patbodin30443@hotmail.com>
2021-10-02 13:38:41 +0700

    Include dotenv as a config file & create .sample-env as config guideline

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const chalk = require("chalk");
+const dotenv = require("dotenv");
 
 const app = express();
+
+dotenv.config();
+
+let port = 5000;
 
 app.get("/api/v1/testapi", () => {
     console.log(chalk.blue("Test console successful!!"));
@@ -11,6 +16,9 @@ app.get("/api/v2/testapi", (req, res) => {
     res.send("Test API successful!!");
 })
 
-app.listen(5000, () => {
-    console.log(chalk.bold.green("Backend server started successfully!!!"));
+app.listen(process.env.SERVER_PORT || port, () => {
+    if(process.env.SERVER_PORT)
+        console.log(chalk.bold.green(`Backend server started successfully!!! on PORT ${process.env.SERVER_PORT}`));
+    else
+        console.log(chalk.bold.green(`Backend server started successfully!!! on PORT ${port}`));
 })
