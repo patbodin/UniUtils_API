9964a0a7da778cd41d27e36c63114a54f5ac9bee
Patbodin Intajak <patbodii@ais.co.th>
2021-10-15 21:33:24 +0700

    Revise BEServer to support internal server ip

--- commons/Const/BEServer.js
+++ commons/Const/BEServer.js
@@ -1,6 +1,8 @@
 class BEServerConst {
-    static serverDomain = "http://localhost";
-    static serverPort = "3000";
+    // static serverDomain = "http://localhost";
+    // static serverPort = "3000";
+    static serverDomain = "http://10.138.46.91";
+    static serverPort = "5199";
     static serverFullDomain = `${this.serverDomain}:${this.serverPort}`;
 }
 
