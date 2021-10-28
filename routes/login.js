c62c078589822b117d012d1d52d59e5bcc966522
Patbodin Intajak <patbodii@ais.co.th>
2021-10-28 13:43:38 +0700

    Add more information in log

--- routes/login.js
+++ routes/login.js
@@ -27,9 +27,13 @@ const logger = require("../features/logging/logging_handler");
 route.post('/v1/validatelogin', async (req, res) => {
     logger.info("+++ Login Validation Start +++");
     logger.info("Username: " + req.body.username);
+    logger.info("Req Body: " + JSON.stringify(req.body));
+    logger.info("Req Headers: " + JSON.stringify(req.headers));
     // const oResult = "" + req.body.username + req.body.password;
     const oResult = await loginHandler.validateLogin(req.body.username, req.body.password);
     
+    logger.info("Resp Body: " + JSON.stringify(oResult.data));
+    logger.info("Resp Headers: " + JSON.stringify(oResult.headers));
     logger.info("Result: " + oResult.data.result);
     logger.info("Process datetime: " + oResult.data.datetime);
     logger.info("+++ Login Validation End +++");
