4184dbb5dafd36fe56ef5f9363ded80926a2c020
Patbodin Intajak <patbodii@ais.co.th>
2021-10-28 13:37:45 +0700

    Include winston-daily-rotate-file to rotate periodically

--- features/logging/logging_handler.js
+++ features/logging/logging_handler.js
@@ -1,12 +1,31 @@
 const { createLogger, format, transports } = require('winston');
+require('winston-daily-rotate-file');
+
+// module.exports = createLogger({
+// transports:
+//     new transports.File({
+//     filename: 'logs/server.log',
+//     format:format.combine(
+//         format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+//         format.align(),
+//         format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+//     )}),
+// // });
 
 module.exports = createLogger({
-transports:
-    new transports.File({
-    filename: 'logs/server.log',
-    format:format.combine(
-        format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-        format.align(),
-        format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-    )}),
-});
+    transports: [
+        new transports.DailyRotateFile({
+           filename: 'logs/info-%DATE%.log',
+           datePattern: 'YYYY-MM-DD_HH-mm',
+           zippedArchive: true,
+           maxSize: '10m',
+           frequency: '30m',
+           level: 'info',
+           format:format.combine(
+                format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+                format.align(),
+                format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+            )
+        })
+      ]
+    });
