c520bc72883f565d6100f8238643c979b5f0cf15
Patbodin Intajak <patbodii@ais.co.th>
2021-12-28 13:39:12 +0700

    Set access-control-allow-origin to *

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let port = 5000;
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers','Content-Type,Accept')
     next()
